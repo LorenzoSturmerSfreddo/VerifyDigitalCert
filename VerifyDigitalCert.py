770312b37b818fe337afca5ceba1a3b99c10360a
LorenzoSturmerSfreddo <114198939+LorenzoSturmerSfreddo@users.noreply.github.com>
2022-11-26 17:41:32 -0300

    Update VerifyDigitalCert.py
    Finalização do código para envio.

--- VerifyDigitalCert.py
+++ VerifyDigitalCert.py
@@ -5,22 +5,10 @@ from cert_chain_resolver.resolver import resolve
 
 store = OpenSSL.crypto.X509Store()
 
-#Esta função não é necessária pois podemos fazer a comparação dos certificados dentro da função de validação;
-"""
-def loadTrusted():
-    certificados = os.listdir("./trusted/")
-    for it in certificados:
-        with open("./trusted/" + it, "rb") as c:
-            chain = resolve(c.read())
-            for cert in chain:
-                if cert.is_root:
-                    store.add_cert(cert)
-"""
-
 #Esta função adiciona o certificado ROOT à lista de certificados confiáveis. Utilizando desta forma, podemos pegar a CA de qualquer certificado que
 #desejarmos da internet e dizer q a CA dele é confíavel;
 def addTrustedCertificate():
-    path = input("Insira o caminho do certificado raiz:\n")
+    path = input("\nCaminho do certificado ROOT:\n")
     if os.path.isfile(path):
         if path.endswith(".crt") or path.endswith(".cer") or path.endswith(".pem"):
             with open(path,'rb') as c:
@@ -28,13 +16,14 @@ def addTrustedCertificate():
                 for cert in chain:
                     if cert.is_root:
                         shutil.copy(path, "./trusted/")
+                        print("Certificado ROOT adicionado a lista de confiança!")
     else:
-        print("Arquivo não encontrado")
+        print("\nCertificado não encontrado!")
 
 
 #Esta função compara o certificado CA para verificar se ele consta na lista de certificados confiáveis;
 def validateCertificate():
-    path = input("Insira o caminho do arquivo:\n")
+    path = input("\nCaminho do certificado:\n")
     if os.path.isfile(path):
         if path.endswith(".crt") or path.endswith(".cer") or path.endswith(".pem"):
             if path.endswith(".crt") or path.endswith(".cer") or path.endswith(".pem"):
@@ -47,25 +36,24 @@ def validateCertificate():
                                 for it in certificados:
                                     with open("./trusted/" + it, "rb") as trusted:
                                         chain_cert = resolve(trusted.read())
-                                        for cert_val in chain:
-                                            if cert_val.is_root and cert_val == cert:
-                                                print("Certificado confiavel\n" + cert_val)
+                                        for cert_val in chain_cert:
+                                            if cert_val.is_root and str(cert) == str(cert_val):
+                                                print("\nCertificado confiavel!\n" + str(cert_val))
+                                            elif cert_val.is_root and str(cert) != str(cert_val):
+                                                print("\nCertificado não confiavel!\n" + str(cert_val))
                             except OpenSSL.crypto.X509StoreContextError as e:
-                                print("Certificado não confiavel\n" + cert_val)
+                                print("\nCertificado não confiavel!\n" + cert_val)
 
     else:
-        print("Arquivo não encontrado")
+        print("\nCertificado não encontrado!")
 
-#loadTrusted()
 opc = 0
 while opc != 4:
-    print()
-    print("--------- Validar confiança de certificados ---------")
-    print("1 - Adicionar ACR")
-    print("2 - Validar confiança de certificado")
-    print("3 - Sair")
+    print("\n********* Menu *********")
+    print("1 -> Adicionar AC ROOT à lista de confiança.")
+    print("2 -> Validar confiança de um certificado.")
+    print("3 -> Sair.")
     opc = int(input())
-    print()
     if opc == 1:
         addTrustedCertificate()
     elif opc == 2:
